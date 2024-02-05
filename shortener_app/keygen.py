10d705994ab55183b79e2541a8dcb4880699d0f3
east4ming <cuikaidong@foxmail.com>
2024-02-05 09:27:06 +0800

    add keygen.py

---
+++ shortener_app/keygen.py
@@ -0,0 +1,10 @@
+import secrets
+import string
+
+
+def create_random_key(length: int = 5) -> str:
+    """
+    Creates a random key of a given length.
+    """
+    chars = string.ascii_uppercase + string.digits
+    return "".join(secrets.choice(chars) for _ in range(length))
